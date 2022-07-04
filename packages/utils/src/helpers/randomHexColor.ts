dc4bc2bc9574122261a6929bc5f5288c1317ba06
Новиков Игорь <novikovio74@gmail.com>
2022-07-04 14:59:54 +0500

    fix(#83): add 0 to the end in randomHexColor when color is cropped

--- packages/utils/src/helpers/randomHexColor.ts
+++ packages/utils/src/helpers/randomHexColor.ts
@@ -1,1 +1,2 @@
-export const randomHexColor = () => `#${((Math.random() * 0xffffff) << 0).toString(16)}`;
+export const randomHexColor = () =>
+  `#${((Math.random() * 0xffffff) << 0).toString(16)}`.padEnd(7, "0");
